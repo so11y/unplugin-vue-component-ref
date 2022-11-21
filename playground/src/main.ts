0891bf3c9c789ca1a0c0c78d44a46d72301c030c
洪鹏程 <danieleuoov@163.com>
2022-11-21 13:20:58 +0800

    update: 修改依赖

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './app';
+import unpluginVueRef from '../../client_/index';
 
-createApp(App).mount('#app')
+createApp(App).use(unpluginVueRef);
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { vitePlugin } from '../node_/index';
+import type { FilterPattern } from '@rollup/pluginutils';
+interface Options {
+	include?: FilterPattern;
+	exclude?: FilterPattern;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(),vitePlugin({ /* options */ }) ]
 })
